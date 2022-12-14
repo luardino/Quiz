a9c322f4388a32a8fb436af7747df27e5f9c6805
Luardino Eduardo Lopes Gaspar Jaime <luardino@icloud.com>
2022-12-14 23:09:26 +0100

    First commit

---
+++ JS/questions.js
@@ -0,0 +1,44 @@
+export default[
+    {
+        question: "Em que ano Angola se tornou independente?",
+        answers: [
+                {option:"2002", correct: false},
+                {option:"1979", correct: false},
+                {option:"1975", correct: true},
+                {option:"1961", correct: false},
+        ],
+    },
+
+    {
+        question: "Em que ano Angola se tornou independente?",
+        answers: [
+                {option:"2002", correct: false},
+                {option:"1979", correct: false},
+                {option:"1975", correct: true},
+                {option:"1961", correct: false},
+        ],
+    },
+
+    {
+        question: "Em que ano Angola se tornou independente?",
+        answers: [
+                {option:"2002", correct: false},
+                {option:"1979", correct: false},
+                {option:"1975", correct: true},
+                {option:"1961", correct: false},
+        ],
+    },
+
+    {
+        question: "Em que ano Angola se tornou independente?",
+        answers: [
+                {option:"2002", correct: false},
+                {option:"1979", correct: false},
+                {option:"1975", correct: true},
+                {option:"1961", correct: false},
+        ],
+    },
+
+
+
+]
